f758b98b970655cb28708ca4514a42a9ab6dbf56
Sachin Venugopalan <sachinvenugopalan99@gmail.com>
2023-04-22 11:08:56 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import  Home  from './containers/home/home';
 import Cart from './containers/cart/cart';
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from "react-redux";
 import { store } from './redux/store/store';
 
@@ -11,12 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
     <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/carts' element={<Cart />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
